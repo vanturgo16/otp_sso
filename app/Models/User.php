7a5b81f759f968cb2e4f2247e7278124db0fb397
fauzi ahmad <ozanfauzi39@gmail.com>
2024-07-17 17:48:04 +0700

    Update pada User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
