9b175650c9770a1e4b90e6a6cfdabba19d9a13d7
fauzi ahmad <ozanfauzi39@gmail.com>
2024-07-22 21:08:30 +0700

    permission akses v1

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Spatie\Permission\PermissionRegistrar;
 class MenuController extends Controller
 {
     public function index()
     {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
         return view('menusso.menu');
     }
 }
